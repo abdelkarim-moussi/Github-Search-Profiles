9b7a6609c3e3a4cae448031177be61e28e7fb662
Abdelkarim Moussi <abdelkarimmoussi73@gmail.com>
2024-11-23 17:23:08 +0100

    fetching users informations

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,15 +3,14 @@ let changeThemeButton = document.getElementById("change-theme-btn");
 var themeTitle = document.getElementById("theme");
 var themeIcon = document.getElementById("theme-icon");
 var userImage = document.getElementById("user-image");
-const searchArea= document.querySelector("#search-area")
-const searchBytton= document.querySelector("#search-btn")
+const searchArea = document.querySelector("#search-area")
+const searchButton= document.querySelector("#search-btn")
+const form = document.querySelector("#form");
 
-console.log(searchArea.value);
 
 function LightTheme(){
     if(themeIcon.src = "./assets/imgs/icon-sun.svg"){
         themeIcon.src = "./assets/imgs/icon-moon.svg"
-
         themeTitle.innerHTML = "Light"
     }
 }
@@ -32,19 +31,39 @@ changeThemeButton.addEventListener("click",LightTheme,darkTheme);
 // )
 // .then(data=>console.log(data));
 
-async function fetchUsers(){
-    try{
-        const responce = await fetch("https://api.github.com/users");
-        const users = await responce.json()
-        console.log(users);
 
-        users.map((user)=>{
-            userImage.src = user.avatar_url;
-         console.log(user.avatar_url);
-        })
+searchButton.addEventListener("click",e =>{
+    e.preventDefault();
+
+    let searchValue = ((searchArea.value).toLowerCase()).replace(/\s/g,''); 
+
+    async function fetchUsers(){
+        try{
+            const responce = await fetch("https://api.github.com/users");
+            const users = await responce.json();
+
+            // console.log(users);
+            // console.log(searchValue)
+            users.map((user)=>{
+                if(user.login.toLowerCase() === searchArea.value){
+                    console.log("login",user.id)
+                    console.log("true");
+                  }
+                  
+                  else{
+                    console.log("not found");
+                  }
+                  
+                 console.log("S type",typeof(searchValue))
+                 console.log("L type",typeof(user.login))
+            
+            })
+              
+        }
+        catch(error){
+            console.log(error);
+        }
     }
-    catch(error){
-        console.log(error);
-    }
-}
-fetchUsers();
+    fetchUsers();
+    
+})
